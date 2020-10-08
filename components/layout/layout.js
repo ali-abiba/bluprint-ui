4b5a3457df541228e6cd346d66999be1499ec407
ali.abiba <ali.abiba298@gmail.com>
2020-10-08 18:44:19 -0400

    Add icon for the home page

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -5,7 +5,9 @@ const Layout = (props) => {
             <Head>
                 <title>BluPrint Art</title>
                 <script src="https://kit.fontawesome.com/a5a13ba575.js" crossOrigin="anonymous"></script>
-                <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Pathway+Gothic+One&family=Saira+Condensed:wght@300&display=swap" rel="stylesheet"/>            </Head>
+                <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Pathway+Gothic+One&family=Saira+Condensed:wght@300&display=swap" rel="stylesheet"/>
+                <link rel="icon" href="public/logo.png" />
+            </Head>
             {props.children}
         </div>
     )
