3716ce9102e110172d805b958c188741807dccd6
ali.abiba <ali.abiba298@gmail.com>
2020-10-08 20:20:40 -0400

    Add mobile formatting

--- components/imageGrid/imageGrid.js
+++ components/imageGrid/imageGrid.js
@@ -9,26 +9,25 @@ export default function ImageGrid(props) {
 
     return (
         <div className={styles.grid}>
-            <div className={`${styles.topLeft} ${styles.cell}`} style={getCellStyle("top-left.png")}>
+            <div className={`${styles.topLeft} ${styles.cell}`} style={getCellStyle("imageGrid/top-left.png")}>
             </div>
-            <div className={`${styles.mid} ${styles.cell}`} style={getCellStyle("mid.png")}>
+            <div className={`${styles.mid} ${styles.cell}`} style={getCellStyle("imageGrid/mid.png")}>
             </div>
-            <div className={`${styles.bottomLeft} ${styles.cell}`} style={getCellStyle("bottom-left.png")}>
+            <div className={`${styles.bottomLeft} ${styles.cell}`} style={getCellStyle("imageGrid/bottom-left.png")}>
             </div>
-            <div className={`${styles.bottomMid} ${styles.cell}`} style={getCellStyle("bottom-mid.png")}>
+            <div className={`${styles.bottomMid} ${styles.cell}`} style={getCellStyle("imageGrid/bottom-mid.png")}>
             </div>
-            <div className={`${styles.topMid} ${styles.cell}`} style={getCellStyle("top-mid.png")}>
+            <div className={`${styles.topMid} ${styles.cell}`} style={getCellStyle("imageGrid/top-mid.png")}>
             </div>
-            <div className={`${styles.topRight} ${styles.cell}`} style={getCellStyle("top-right.png")}>
+            <div className={`${styles.topRight} ${styles.cell}`} style={getCellStyle("imageGrid/top-right.png")}>
             </div>
-            <div className={`${styles.bottomRight} ${styles.cell}`} style={getCellStyle("bottom-right.png")}>
+            <div className={`${styles.bottomRight} ${styles.cell}`} style={getCellStyle("imageGrid/bottom-right.png")}>
             </div>
             <div className={`${styles.center}`}>
                 <div className={styles.centerContent}>
                 <img src="/logo.png" className={styles.logo}/>
             </div>
                 <h2 className={styles.text}>Coming Soon</h2>
-
             </div>
         < /div>
     )
